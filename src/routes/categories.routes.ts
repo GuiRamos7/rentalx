b61ac145218f9a3a2bcb45a553dd91d09267b2c1
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-08-22 22:51:43 -0300

    feat: creating useCases for our import category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
@@ -19,12 +20,7 @@ categoriesRoutes.get("/", (_, res) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
-  const { file } = req;
-
-  console.log(file);
-
-  return res.send();
-  // return createCategoryController.handle(req, res);
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
