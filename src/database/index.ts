71df945040c4c955384b4dc4a86cb4f95806ee5a
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-09-28 14:59:14 -0300

    feat: config docker

---
+++ src/database/index.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "docker",
+  password: "test",
+  database: "rentx",
+});
+
+AppDataSource.initialize();
